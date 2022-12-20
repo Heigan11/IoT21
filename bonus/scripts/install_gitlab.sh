97853aca7ae2112b63768dfa58ccd4d83804afcb
Jraye <heigan11@gmail.com>
2022-12-20 21:48:24 +0300

    bonus chng

--- bonus/scripts/install_gitlab.sh
+++ bonus/scripts/install_gitlab.sh
@@ -2,9 +2,8 @@
 
 sudo apt-get install -y curl openssh-server ca-certificates tzdata perl
 
-curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 
-sudo EXTERNAL_URL="http://gitlab.example.com" apt-get install gitlab-ee
-#sudo EXTERNAL_URL="http://192.168.56.110" apt-get install -y gitlab-ce
+sudo EXTERNAL_URL="http://10.0.2.15" apt-get install -y gitlab-ce
 
 sudo cat /etc/gitlab/initial_root_password
